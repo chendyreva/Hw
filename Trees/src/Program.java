052c19f9fae7ff8a660692cc73d3c1d974ebfdd4
chendyreva <nat-217@mail.ru>
2018-12-14 21:15:59 +0500

    Trees

---
+++ Trees/src/Program.java
@@ -0,0 +1,19 @@
+
+public class Program {
+    public static  void main(String[] args) {
+        BST<Character, Integer> assArr = new BST<>();
+        assArr.put('D', 288);
+        assArr.put('K', 344);
+        assArr.put('M', 747);
+        assArr.put('A', 344);
+        assArr.put('L', 844);
+        assArr.put('N', 555);
+        assArr.put('W', 612);
+        assArr.put('O', 48);
+        assArr.put('T', 278);
+        assArr.put('R', 212);
+        assArr.put('Q', 700);
+        assArr.put('Z', 000);
+        System.out.println();
+    }
+}
